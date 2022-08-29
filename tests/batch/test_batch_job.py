45d026e598eb791b485e8eeb774dbedfe3c85266
Chris Gregory <chris@hume.ai>
2022-08-29 21:24:08 +0000

    BatchJob get_status and get_result

--- tests/batch/test_batch_job.py
+++ tests/batch/test_batch_job.py
@@ -1,26 +1,48 @@
-from unittest.mock import MagicMock
+from unittest.mock import Mock
 
 import pytest
-from pytest import MonkeyPatch
 
-from hume import BatchJob, HumeBatchClient
+from hume import BatchJob, BatchJobResult, BatchJobStatus, ModelType
+from hume.common.config import FaceConfig
 
 
 @pytest.fixture(scope="function")
-def batch_client(monkeypatch: MonkeyPatch) -> HumeBatchClient:
-    mock_start_job = MagicMock(return_value="fake-job")
-    monkeypatch.setattr(HumeBatchClient, "start_job", mock_start_job)
-    return HumeBatchClient("0000-0000-0000-0000")
+def batch_client() -> Mock:
+    mock_client = Mock()
+    job_result = BatchJobResult(
+        configs={
+            ModelType.FACE: FaceConfig(),
+        },
+        urls="mock-url",
+        status=BatchJobStatus.FAILED,
+    )
+    mock_client.get_job_result = Mock(return_value=job_result)
+    return mock_client
 
 
 class TestBatchJob:
 
-    def test_job_id(self, batch_client: HumeBatchClient):
+    def test_job_id(self, batch_client: Mock):
         mock_job_id = "mock-job-id"
         job = BatchJob(batch_client, mock_job_id)
         assert job.id == mock_job_id
 
-    def test_invalid_await_timeout(self, batch_client: HumeBatchClient):
-        job = BatchJob(batch_client, "mock_job_id")
+    def test_invalid_await_timeout(self, batch_client: Mock):
+        job = BatchJob(batch_client, "mock-job-id")
         with pytest.raises(ValueError, match="timeout must be at least 1 second"):
             job.await_complete(timeout=0)
+
+    def test_get_result(self, batch_client: Mock):
+        job = BatchJob(batch_client, "mock-job-id")
+        result = job.get_result()
+        assert result.status == BatchJobStatus.FAILED
+
+    def test_get_status(self, batch_client: Mock):
+        job = BatchJob(batch_client, "mock-job-id")
+        status = job.get_status()
+        assert status == BatchJobStatus.FAILED
+
+    def test_await_complete(self, batch_client: Mock):
+        job = BatchJob(batch_client, "mock-job-id")
+        result = job.await_complete()
+        assert result.status == BatchJobStatus.FAILED
