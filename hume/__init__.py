c5b9afd1076813a479cefb41baf41e1bf903ee6c
Chris Gregory <chris@hume.ai>
2022-09-15 01:11:40 +0000

    Fix broken init

--- hume/__init__.py
+++ hume/__init__.py
@@ -2,10 +2,9 @@
 import importlib.metadata
 
 from hume._batch import BatchJob, BatchJobResult, BatchJobStatus, HumeBatchClient
-from hume.common.hume_client_error import HumeClientError
+from hume._common.hume_client_error import HumeClientError
 from hume._common.model_type import ModelType
 
-_common
 __version__ = importlib.metadata.version("hume")
 
 __all__ = [
