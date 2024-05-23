f35c3c1618924e923d33dcc4895902925acac039
Maxwell Muoto <41130755+max-muoto@users.noreply.github.com>
2024-05-23 18:56:48 -0500

    revert

--- hume/models/config/model_config_base.py
+++ hume/models/config/model_config_base.py
@@ -2,13 +2,16 @@
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from typing import Generic, TypeVar
 
 from hume._common.config_base import ConfigBase
 from hume.models import ModelType
 
+T = TypeVar("T")  # Type for subclasses of ModelConfigBase
+
 
 @dataclass
-class ModelConfigBase(ConfigBase["ModelConfigBase"], ABC):
+class ModelConfigBase(ConfigBase["ModelConfigBase"], ABC, Generic[T]):
     """Abstract base class for model configurations."""
 
     @classmethod
