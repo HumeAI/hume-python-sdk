5e55e6e982080037b1fae663f6dc16a5648df1e9
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-06-06 03:35:07 +0000

    SDK regeneration

--- src/hume/core/client_wrapper.py
+++ src/hume/core/client_wrapper.py
@@ -17,7 +17,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "hume",
-            "X-Fern-SDK-Version": "1.0.0",
+            "X-Fern-SDK-Version": "0.0.46",
         }
         if self.api_key is not None:
             headers["X-Hume-Api-Key"] = self.api_key
