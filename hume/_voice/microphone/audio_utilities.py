9bb56cfa0dbde423e2ef8fd711cfe813034f50ce
Norman Bukingolts <norman@hume.ai>
2024-06-17 17:08:36 -0700

    Updates audio_utilities.py to remove several linting errors

--- hume/_voice/microphone/audio_utilities.py
+++ hume/_voice/microphone/audio_utilities.py
@@ -6,8 +6,8 @@ from io import BytesIO
 import pydub.playback
 from pydub import AudioSegment
 
-playback_object = None
-stop_event = asyncio.Event()
+_playback_object = None
+_stop_event = asyncio.Event()
 
 # NOTE:
 # - expects that byte_str is a valid audio file with the appropriate headers
@@ -24,23 +24,24 @@ async def play_audio(byte_str: bytes) -> None:
     Args:
         byte_str (bytes): Byte string of audio data.
     """
-    global playback_object, stop_event
+    global _playback_object, _stop_event
 
-    stop_event.clear()  # Reset the stop event
+    _stop_event.clear()  # Reset the stop event
 
     segment = AudioSegment.from_file(BytesIO(byte_str))
 
     def _play_audio_segment() -> None:
-        global playback_object
-        playback_object = pydub.playback.play(segment)
+        global _playback_object
+        _playback_object = pydub.playback.play(segment)
 
     await asyncio.to_thread(_play_audio_segment)
 
 
-async def stop_audio() -> None:
+def stop_audio() -> None:
     """Stop the current audio playback."""
-    global playback_object, stop_event
-    stop_event.set()  # Set the stop event to signal stopping
+    global _playback_object, _stop_event
+    _stop_event.set()  # Set the stop event to signal stopping
 
-    if playback_object and playback_object.is_playing():
-        playback_object.stop()
+    if _playback_object:
+        _playback_object.stop()
+        _playback_object = None
