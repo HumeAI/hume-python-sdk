906a7fa19865dee7d9cbfe0a4a8cac7c71b16c4e
Norman Bukingolts <norman@hume.ai>
2024-06-17 22:54:29 -0700

    Explicitly uses simpleaudio for audio playback (again)

--- hume/_voice/microphone/audio_utilities.py
+++ hume/_voice/microphone/audio_utilities.py
@@ -1,23 +1,20 @@
-"""Utility class for audio playback."""
+"""Utilities for audio playback."""
 
 import asyncio
 from io import BytesIO
+from typing import Optional
 
-import pydub.playback
+import simpleaudio as sa
 from pydub import AudioSegment
 
 # NOTE:
 # - expects that byte_str is a valid audio file with the appropriate headers
-# - implicitly relies on simpleaudio OR pyaudio OR ffmpeg, defaulting in that order.
-#   - [https://stackoverflow.com/a/20746883]
-#   - [https://github.com/jiaaro/pydub#playback]
-#   - [https://github.com/jiaaro/pydub/blob/master/pydub/playback.py]
-# - stop_audio() allows for the decoupling of interruptibility from the MicrophoneSender class.
+# - explicitly uses simpleaudio for audio playback
 
 
 class AudioPlayer:
     def __init__(self) -> None:
-        self._playback_object = None
+        self._playback_object: Optional[sa.PlayObject] = None
         self._stop_event = asyncio.Event()
 
     async def play_audio(self, byte_str: bytes) -> None:
@@ -29,9 +26,14 @@ class AudioPlayer:
         self._stop_event.clear()  # Reset the stop event
 
         segment = AudioSegment.from_file(BytesIO(byte_str))
+        audio_data = segment.raw_data
+        num_channels = segment.channels
+        bytes_per_sample = segment.sample_width
+        sample_rate = segment.frame_rate
 
         def _play_audio_segment() -> None:
-            self._playback_object = pydub.playback.play(segment)
+            wave_obj = sa.WaveObject(audio_data, num_channels, bytes_per_sample, sample_rate)
+            self._playback_object = wave_obj.play()
 
         await asyncio.to_thread(_play_audio_segment)
 
