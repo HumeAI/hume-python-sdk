3b1d550ff850c095073c715e5bb39ea05df7757e
Norman Bukingolts <norman@hume.ai>
2024-06-17 17:00:30 -0700

    Removes simpleaudio as explicit dependency

--- hume/_voice/microphone/audio_utilities.py
+++ hume/_voice/microphone/audio_utilities.py
@@ -3,16 +3,15 @@
 import asyncio
 from io import BytesIO
 
-import simpleaudio
+import pydub.playback
 from pydub import AudioSegment
 
 playback_object = None
 stop_event = asyncio.Event()
 
-
 # NOTE:
 # - expects that byte_str is a valid audio file with the appropriate headers
-# - implicitly relies on simpleaudio:
+# - implicitly relies on simpleaudio OR pyaudio OR ffmpeg, defaulting in that order.
 #   - [https://stackoverflow.com/a/20746883]
 #   - [https://github.com/jiaaro/pydub#playback]
 #   - [https://github.com/jiaaro/pydub/blob/master/pydub/playback.py]
@@ -33,13 +32,7 @@ async def play_audio(byte_str: bytes) -> None:
 
     def _play_audio_segment() -> None:
         global playback_object
-        playback_object = simpleaudio.play_buffer(
-            segment.raw_data,
-            num_channels=segment.channels,
-            bytes_per_sample=segment.sample_width,
-            sample_rate=segment.frame_rate,
-        )
-        playback_object.wait_done()
+        playback_object = pydub.playback.play(segment)
 
     await asyncio.to_thread(_play_audio_segment)
 
