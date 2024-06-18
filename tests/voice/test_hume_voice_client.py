6a24e6c8a358f46c525d21dda68a52c5be58e59a
Norman Bukingolts <norman@hume.ai>
2024-06-17 23:46:02 -0700

    Attempts to edit test to support async iterator

--- tests/voice/test_hume_voice_client.py
+++ tests/voice/test_hume_voice_client.py
@@ -1,6 +1,6 @@
 import logging
 from contextlib import asynccontextmanager
-from typing import AsyncContextManager, AsyncIterator, Optional
+from typing import Any, AsyncContextManager, AsyncIterator, Optional
 from unittest.mock import Mock
 
 import pytest
@@ -11,6 +11,18 @@ from hume import HumeVoiceClient
 from hume._voice.voice_socket import VoiceSocket
 
 logger = logging.getLogger(__name__)
+
+
+# Custom async iterator mock class
+class AsyncIteratorMock:
+    def __init__(self, return_value: Any):
+        self._return_value = return_value
+
+    def __aiter__(self) -> AsyncIterator[Any]:
+        return self
+
+    async def __anext__(self) -> Any:
+        return self._return_value
 
 
 # pylint: disable=unused-argument
@@ -30,7 +42,9 @@ def mock_connect(
 
     @asynccontextmanager
     async def mock_connection() -> AsyncIterator[Mock]:
-        yield Mock()
+        mock_protocol = Mock()
+        mock_protocol.__aiter__ = lambda: AsyncIteratorMock(Mock())
+        yield mock_protocol
 
     return mock_connection()
 
