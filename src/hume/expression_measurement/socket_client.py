c8382f0bd6c035420841844ff699ece331bbe45b
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-08-12 19:30:33 -0400

    add file utils

--- src/hume/expression_measurement/socket_client.py
+++ src/hume/expression_measurement/socket_client.py
@@ -1,3 +1,4 @@
+from hume.expression_measurement.batch.client_with_utils import AsyncBatchClientWithUtils, BatchClientWithUtils
 from .client import AsyncExpressionMeasurementClient, ExpressionMeasurementClient
 from .stream.socket_client import AsyncStreamClientWithWebsocket
 
@@ -5,6 +6,7 @@ from .stream.socket_client import AsyncStreamClientWithWebsocket
 class ExpressionMeasurementClientWithWebsocket(ExpressionMeasurementClient):
     def __init__(self, *, client_wrapper):
         super().__init__(client_wrapper=client_wrapper)
+        self.batch = BatchClientWithUtils(client_wrapper=client_wrapper)
     
     @property
     def stream(self) -> None:
@@ -13,4 +15,5 @@ class ExpressionMeasurementClientWithWebsocket(ExpressionMeasurementClient):
 class AsyncExpressionMeasurementClientWithWebsocket(AsyncExpressionMeasurementClient):
     def __init__(self, *, client_wrapper):
         super().__init__(client_wrapper=client_wrapper)
+        self.batch = AsyncBatchClientWithUtils(client_wrapper=client_wrapper)
         self.stream = AsyncStreamClientWithWebsocket(client_wrapper=client_wrapper)
