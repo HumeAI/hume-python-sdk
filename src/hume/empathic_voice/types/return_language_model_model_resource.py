7cca37cf527e286a88902a4358f41a74b73a25b8
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-11-06 20:21:02 +0000

    SDK regeneration

--- src/hume/empathic_voice/types/return_language_model_model_resource.py
+++ src/hume/empathic_voice/types/return_language_model_model_resource.py
@@ -5,7 +5,9 @@ import typing
 ReturnLanguageModelModelResource = typing.Union[
     typing.Literal[
         "claude-3-5-sonnet-latest",
+        "claude-3-5-haiku-latest",
         "claude-3-5-sonnet-20240620",
+        "claude-3-5-haiku-20241022",
         "claude-3-opus-20240229",
         "claude-3-sonnet-20240229",
         "claude-3-haiku-20240307",
