9b1195495dc24125db37adecdbdd8584b633c93b
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-06-02 19:07:37 +0000

    SDK regeneration

--- src/hume/expression_measurement/batch/types/transcription.py
+++ src/hume/expression_measurement/batch/types/transcription.py
@@ -8,48 +8,7 @@ from ....core.pydantic_utilities import IS_PYDANTIC_V2
 
 
 class Transcription(UniversalBaseModel):
-    """
-    Transcription-related configuration options.
-
-    To disable transcription, explicitly set this field to `null`.
-    """
-
-    language: typing.Optional[Bcp47Tag] = pydantic.Field(default=None)
-    """
-    By default, we use an automated language detection method for our Speech Prosody, Language, and NER models. However, if you know what language is being spoken in your media samples, you can specify it via its BCP-47 tag and potentially obtain more accurate results.
-    
-    You can specify any of the following languages:
-    - Chinese: `zh`
-    - Danish: `da`
-    - Dutch: `nl`
-    - English: `en`
-    - English (Australia): `en-AU`
-    - English (India): `en-IN`
-    - English (New Zealand): `en-NZ`
-    - English (United Kingdom): `en-GB`
-    - French: `fr`
-    - French (Canada): `fr-CA`
-    - German: `de`
-    - Hindi: `hi`
-    - Hindi (Roman Script): `hi-Latn`
-    - Indonesian: `id`
-    - Italian: `it`
-    - Japanese: `ja`
-    - Korean: `ko`
-    - Norwegian: `no`
-    - Polish: `pl`
-    - Portuguese: `pt`
-    - Portuguese (Brazil): `pt-BR`
-    - Portuguese (Portugal): `pt-PT`
-    - Russian: `ru`
-    - Spanish: `es`
-    - Spanish (Latin America): `es-419`
-    - Swedish: `sv`
-    - Tamil: `ta`
-    - Turkish: `tr`
-    - Ukrainian: `uk`
-    """
-
+    language: typing.Optional[Bcp47Tag] = None
     identify_speakers: typing.Optional[bool] = pydantic.Field(default=None)
     """
     Whether to return identifiers for speakers over time. If `true`, unique identifiers will be assigned to spoken words to differentiate different speakers. If `false`, all speakers will be tagged with an `unknown` ID.
